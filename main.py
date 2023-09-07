e10c5ab3696187b551858e33795d52b73434706d
Esteria04 <huijunyi0212@naver.com>
2023-09-07 11:04:58 +0900

    .

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from dotenv import load_dotenv
 import os
 from pydantic import BaseModel
-from sqlalchemy import create_engine, Column, Integer, DateTime , String
+from sqlalchemy import create_engine, Column, Integer, DateTime
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
